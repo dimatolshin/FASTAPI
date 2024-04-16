1d35932ad69eb7928e81ebe3e022067d6d0970b8
dima_tolshin <dima_tolshin@mail.ru>
2024-04-16 22:43:58 +0300

    commit1

---
+++ Api/main.py
@@ -0,0 +1,75 @@
+from fastapi import FastAPI, Body, status, Depends
+from fastapi.responses import JSONResponse, FileResponse
+from MainApi.SQLAlchemi.database import *
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get('/')
+def main():
+    return FileResponse('publick/index.html')
+
+
+@app.get('/api/users')
+def all_data(db: Session = Depends(get_db)):
+    return db.query(Person).all()
+
+
+@app.get('/api/users/{id}')
+def get_person(id, db: Session = Depends(get_db)):
+    user = db.query(Person).filter(Person.id == id).first()
+    if user == None:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'message': 'user does not exits'})
+    return user
+
+
+@app.post('/api/users')
+def create_person(user=Body(), db: Session = Depends(get_db)):
+    person = Person(name=user['name'], age=user['age'])
+    if person.name == '' or person.age == None:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'message': 'user does not exits'})
+    else:
+        db.add(person)
+        db.commit()
+        db.refresh(person)
+        return person
+
+
+@app.put("/api/users")
+def edit_person(data=Body()):
+    person = db.query(Person).filter(Person.id == data['id']).first()
+    if not person:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'message': 'user does not exits'})
+    else:
+        person.name = data['name']
+        person.age = data['age']
+        db.commit()
+        db.refresh(person)
+        return person
+
+
+@app.delete('/api/users/{id}')
+def delete_data(id):
+    person = db.query(Person).filter(Person.id == id).first()
+    if person:
+        db.delete(person)
+        db.commit()
+        return person
+    else:
+        JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'message': "user does not exit"})
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8002)
---
+++ Api/tests.py
@@ -0,0 +1,34 @@
+from main import app
+from MainApi.SQLAlchemi.database import *
+from fastapi.testclient import TestClient
+
+Base.metadata.create_all(bind=engine)
+client = TestClient(app)
+person = db.query(Person).filter(Person.name == 'nika').first()
+
+
+def test_read_main():
+    response = client.get("/api/users")
+    assert response.status_code == 200
+    assert response.json() == [{'age': user.age, 'id': user.id, 'name': user.name} for user in db.query(Person).all()]
+
+
+def test_create_user():
+    response = client.post(
+        "/api/users",
+        json={'name': 'nika', 'age': 21})
+    assert response.status_code == 200
+    user = db.query(Person).filter(Person.name == 'nika').first()
+    assert response.json() == {'id': user.id, 'name': user.name, 'age': user.age}
+
+
+def test_delete_user():
+    response = client.delete(f"/api/users/{person.id}")
+    assert response.status_code == 200
+    assert response.json() == {'id': person.id, 'name': person.name, 'age': person.age}
+
+
+def test_get_user():
+    response = client.get(f"/api/users/{person.id}")
+    assert response.status_code == 200
+    assert response.json() == {'id': person.id, 'name': person.name, 'age': person.age}
